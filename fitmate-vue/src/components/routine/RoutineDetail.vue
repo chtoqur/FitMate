37244f71bf4edc2c0c7512f84fd64f1b00bbadfc
Iseo Choi <chtoqur@gmail.com>
2024-05-14 05:43:14 +0900

    루틴 파트 기본 구현

---
+++ fitmate-vue/src/components/routine/RoutineDetail.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h3>{{ store.routine.title }}</h3>
+    <hr>
+    <table>
+      <thead>
+        <tr>
+          <th>운동이름</th>
+          <th>세트</th>
+          <th>횟수</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="course in store.routine.courses" :key="course.name">
+          <td>{{ course.name }}</td>
+          <td>{{ course.set }}</td>
+          <td>{{ course.reps }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router';
+import { useRoutineStore } from '@/stores/routine';
+import { onMounted, ref } from 'vue';
+
+const store = useRoutineStore();
+const route = useRoute();
+
+onMounted(() => {
+  store.getRoutine(route.params.id);
+})
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ fitmate-vue/src/components/routine/RoutineList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div
+      v-for="routine in store.routineList"
+      :key="routine.id"
+      @click="navigateToDetail(routine.id)"
+      class="routine-card"
+    >
+      <div>
+        <span>난이도 {{ routine.level }}</span>
+      </div>
+      <div>
+        <span>{{ routine.title }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+import { useRoutineStore } from '@/stores/routine';
+
+const router = useRouter();
+const store = useRoutineStore();
+
+const navigateToDetail = function(routineId) {
+  router.push({ name: 'routineDetail', params: { id: routineId }})
+}
+</script>
+
+<style scoped>
+.routine-card {
+  border: 1px solid black;
+}
+</style>
