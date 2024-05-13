02b56fa114699947fa9239313af18895102e5d26
이강현 <lkh000131@naver.com>
2024-05-13 21:24:13 +0900

    주소 api 구현

--- fitmate-vue/src/views/SignupView.vue
+++ fitmate-vue/src/views/SignupView.vue
@@ -22,16 +22,52 @@
       <option>하</option>
     </select>
     <p>주소</p>
-    <input type="text" v-model="user.address" />
+    <input
+      type="text"
+      v-model="user.postCode"
+      style="width: 5%"
+      placeholder="우편 번호"
+      disabled
+    />
+    <button @click="postOpen">주소 찾기</button>
+    <br />
+    <input type="text" v-model="address" placeholder="도로명 주소" disabled />
+    <br />
+    <input type="text" v-model="detailAddress" placeholder="상세 주소" />
+    <br />
     <button @click="store.signUp(user)">회원가입</button>
   </div>
 </template>
 
 <script setup>
 import { useUserStore } from "@/stores/user";
-import { ref } from "vue";
+import { ref, onMounted, watch } from "vue";
 
 const store = useUserStore();
+
+onMounted(() => {
+  const script = document.createElement("script");
+  script.src = "//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js";
+  script.async = true;
+  document.head.appendChild(script);
+});
+
+const postOpen = () => {
+  new daum.Postcode({
+    oncomplete: function (data) {
+      // console.log(data);
+      user.value.postCode = data.zonecode;
+      address.value = data.address;
+    },
+  }).open();
+};
+
+const address = ref("");
+const detailAddress = ref("");
+
+watch([address, detailAddress], ([newAddress, newDetailAddress]) => {
+  user.value.address = `${newAddress} ${newDetailAddress}`;
+});
 
 const user = ref({
   name: "",
@@ -39,6 +75,7 @@ const user = ref({
   password: "",
   age: "",
   ability: "",
+  postCode: "",
   address: "",
 });
 </script>
