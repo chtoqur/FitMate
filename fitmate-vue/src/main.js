3cf3e9e3fd55ef630bde9376b553c32529ccf587
Iseo Choi <chtoqur@gmail.com>
2024-05-15 23:25:00 +0900

    import reset.css

--- fitmate-vue/src/main.js
+++ fitmate-vue/src/main.js
@@ -4,9 +4,12 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+import "@/assets/css/reset.css";
+
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+
 
 app.mount("#app");
