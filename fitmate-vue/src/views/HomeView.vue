7d3da75db06eff90034fdfcd7597786214d38f22
이강현 <lkh000131@naver.com>
2024-05-16 09:44:49 +0900

    카카오맵 추가

--- fitmate-vue/src/views/HomeView.vue
+++ fitmate-vue/src/views/HomeView.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
     <h1>이서왕자 강현공주</h1>
-
-    <h3>+) 옆나라 은혜왕자 승철공주 </h3>
+    <KakaoMap />
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import KakaoMap from "@/components/kakao/KakaoMap.vue";
+</script>
 
 <style scoped>
-*{
+* {
   text-align: center;
 }
 </style>
