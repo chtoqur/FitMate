16a99cd4caf3606e9b81d8bd5d45a7660fbf6b53
Iseo <chxiseo@naver.com>
2024-05-17 17:39:43 +0900

    인기 동영상 캐러셀 생성

--- fitmate-vue/src/stores/home.js
+++ fitmate-vue/src/stores/home.js
@@ -1,11 +1,15 @@
 import { ref, computed } from "vue";
 import { defineStore } from "pinia";
 import { useCommunityStore } from '@/stores/community';
+import { useVideoStore } from "@/stores/video";
 
 export const useHomeStore = defineStore("home", ()=>{
   
   const communityStore = useCommunityStore();
+  const videoStore = useVideoStore();
   const popularPostList = ref([]);
+  const rankedVideos = ref([]);
+
 
   const getPopularPosts = function() {
     popularPostList.value = communityStore.postList.sort((postA, postB)=>{
@@ -13,8 +17,16 @@ export const useHomeStore = defineStore("home", ()=>{
     }).slice(0, 5);
   }
 
+  const getRankedVideos = function() {
+    rankedVideos.value = videoStore.videoList.sort((videoA, videoB)=>{
+      return videoB.viewCnt - videoA.viewCnt;
+    }).slice(0, 5);
+  }
+
   return {
     popularPostList,
     getPopularPosts,
+    rankedVideos,
+    getRankedVideos,
   }
 })
