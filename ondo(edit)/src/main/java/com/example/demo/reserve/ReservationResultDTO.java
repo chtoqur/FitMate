6b264125a2bc37fc9adb9d22d330b40669173a19
iseo <chxiseo@naver.com>
2024-05-20 17:42:45 +0900

    머쓱

--- ondo(edit)/src/main/java/com/example/demo/reserve/ReservationResultDTO.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.reserve;
-
-import lombok.Data;
-
-@Data
-public class ReservationResultDTO {
-    private Integer rsNum;      // 예약자 수
-    private Integer userSeq;    // 예약자 순번
-}
